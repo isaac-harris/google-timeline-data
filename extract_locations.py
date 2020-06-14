5bf49c96ae96217a32320b13ac2d082435d30ce4
Isaac Harris <43283700+isaac-harris@users.noreply.github.com>
2020-06-14 21:23:19 +0100

    Updated comment structure & cleaned code
    Some extra comments were added, and some where changed to be more useful.
    Some of the unnecessary code (like the contents variable) was removed.

--- extract_locations.py
+++ extract_locations.py
@@ -1,35 +1,44 @@
+# 
+
+
 # extract all the text from the disarrayed file
 # split each time the key text (see below) appears
 # ('jsan="7.title">')
 
 
-with open("locations.txt", "r") as fin:
-    
-    contents = fin.read()
+with open("locations.txt", "r") as f_in:
 
-    # tag that predates every instance of a location's name
-    split_contents = contents.split("jsan=\"7.title\">")
+    # tag that prefixes every instance of a location's html section
+    # is used to seperate (and isolate) each name into seperate indexes
+    split_contents = f_in.read().split("jsan=\"7.title\">")
 
     # the header element doesnt contain any locations
     split_contents.pop(0)
 
-    final_list = []
+    # is used to store each scanned name
+    location_name_list = []
 
     # isolating the location names from any other text
     for i in split_contents:
         extraction_list = i.split(">")
         
-        final_list.append(extraction_list[0][:-5])
+        # removes the trailing characters of each extraction_list[index]
+        # to leave just the name
+        location_name_list.append(extraction_list[0][:-5])
 
+    # used to convert the location_name_list into a pseudo-set
     final_list_ordered = []
 
     # remove duplicates while retaining order (unlike the set() method)
-    for i in range(0, len(final_list)):
-        if final_list[i] not in final_list_ordered:
-            final_list_ordered.append(final_list[i])
+    # the set method removes duplicates however it removes
+    # them chronologically, so the 'time spent' measure may not
+    # be conserved in the final file
+    for i in range(0, len(location_name_list)):
+        if location_name_list[i] not in final_list_ordered:
+            final_list_ordered.append(location_name_list[i])
             
-    with open("visited_locations.txt", "w") as fout:
+    with open("visited_locations.txt", "w") as f_out:
         for i in final_list_ordered:
-            fout.write(i + "\n")
+            f_out.write(i + "\n")
     
 
